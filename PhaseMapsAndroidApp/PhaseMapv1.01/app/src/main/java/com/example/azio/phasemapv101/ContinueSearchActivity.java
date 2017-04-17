7607fb8d8794aa27a63ce4d9c31cd390b0b82e4f
sahan thinusha <sahanvijaya@gmail.com>
2017-04-18 00:29:24 +0530

    PhaseMaps Android App first commit

---
+++ PhaseMapsAndroidApp/PhaseMapv1.01/app/src/main/java/com/example/azio/phasemapv101/ContinueSearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.azio.phasemapv101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContinueSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continue_search);
+    }
+}
---
+++ PhaseMapsAndroidApp/PhaseMapv1.01/app/src/main/java/com/example/azio/phasemapv101/SearchRouteActivity.java
@@ -0,0 +1,13 @@
+package com.example.azio.phasemapv101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchRouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_route);
+    }
+}
